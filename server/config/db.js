fb15c9dae7d208db0ca72ad534b71c84c06eb844
Tima Baisalov <baisalov.t98@gmail.com>
2025-02-19 02:05:51 -0600

    Improved MongoDB connection error handling

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is missing in .env file");
+    }
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
