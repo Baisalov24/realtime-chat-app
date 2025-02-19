1364f21418cec1ceea51882245594790a47197be
Tima Baisalov <baisalov.t98@gmail.com>
2025-02-19 02:00:38 -0600

    Integrated authentication routes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import { Server } from "socket.io";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import authRoutes from "./routes/authRoutes.js";
 
 connectDB();
 dotenv.config();
@@ -18,6 +19,7 @@ const io = new Server(server, {
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server is running...");
