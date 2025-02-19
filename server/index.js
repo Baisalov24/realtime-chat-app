85f6663e7ad0f0968138b161502c5d9169cb0d1b
Tima Baisalov <baisalov.t98@gmail.com>
2025-02-19 13:57:56 -0600

    feat: integrate auth routes into server

--- server/index.js
+++ server/index.js
@@ -6,10 +6,9 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 
-
 dotenv.config();
 
-
+connectDB();
 
 const app = express();
 const server = http.createServer(app);
@@ -21,6 +20,7 @@ const io = new Server(server, {
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
@@ -28,13 +28,21 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("New user connected:", socket.id);
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("message", (data) => {
+    console.log(`Message received: ${JSON.stringify(data)}`);
+    io.emit("message", data); 
+  });
+
   socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+    console.log(`User disconnected: ${socket.id}`);
+  });
+
+  socket.on("error", (err) => {
+    console.error(` Socket error: ${err.message}`);
   });
 });
-
-connectDB();
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
